3655d493ec391ab32b8b9fd5cd13334005f61c56
CapOfCave <lars.kecker2009@gmail.com>
2018-11-22 22:29:02 +0100

    UI Theme

--- src/main/Window.java
+++ src/main/Window.java
@@ -3,13 +3,18 @@ package main;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Window extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
 	private JPanel panel;
+	
+	private JButton button;
 	
 	public Window() {
 		super("krasses Programm");
@@ -17,6 +22,11 @@ public class Window extends JFrame{
 		panel.setPreferredSize(new Dimension(700, 500));
 		panel.setBackground(Color.WHITE);
 		panel.setLayout(null);
+		
+		button = new JButton("Hallo");
+		button.setBounds(250, 200, 200, 50);
+		panel.add(button);
+		
 		add(panel);
 		setResizable(false);
 		pack();
@@ -29,5 +39,11 @@ public class Window extends JFrame{
 	
 	public static void main(String[] args) {
 		new Window();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 	}
 }
