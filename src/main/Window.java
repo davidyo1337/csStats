33dfc5c3f04b9b08d5074641ae1342cedf6daa54
CapOfCave <lars.kecker2009@gmail.com>
2018-11-22 21:58:33 +0100

    Window

--- src/main/Window.java
+++ src/main/Window.java
@@ -1,5 +1,33 @@
 package main;
 
-public class Window {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	private JPanel panel;
+	
+	public Window() {
+		super("krasses Programm");
+		panel = new JPanel();
+		panel.setPreferredSize(new Dimension(700, 500));
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(null);
+		add(panel);
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(3);
+		setVisible(true);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new Window();
+	}
 }
