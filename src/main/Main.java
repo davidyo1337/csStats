9918fc7657237c9def8f653e7885a069b535dcad
CapOfCave <lars.kecker2009@gmail.com>
2018-11-23 18:14:04 +0100

    nicht viel

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,20 +2,29 @@ package main;
 
 import java.math.BigInteger;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 		new Window();
 		System.out.println(calculateID("STEAM_0:0:125314026"));
 	}
-	
-	public static String calculateID(String steamid)
-	{
+
+	public static String calculateID(String steamid) {
 		String[] id_split = steamid.split(":");
-			   BigInteger steam64id = new BigInteger("76561197960265728").add(new BigInteger("" + (Integer.parseInt(id_split[2]) * 2))) ;
-			   steam64id  = steam64id.add(new BigInteger("" + id_split[1]));
-			   
-			    return steam64id.toString();
+		BigInteger steam64id = new BigInteger("76561197960265728")
+				.add(new BigInteger("" + (Integer.parseInt(id_split[2]) * 2)));
+		steam64id = steam64id.add(new BigInteger("" + id_split[1]));
+
+		return steam64id.toString();
 	}
-	
+
 }
