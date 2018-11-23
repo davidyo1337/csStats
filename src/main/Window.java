8a2ca18fa9dd5f7bbaaf38fd214367cfa38639ce
CapOfCave <lars.kecker2009@gmail.com>
2018-11-23 18:24:45 +0100

    Panels

--- src/main/Window.java
+++ src/main/Window.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
@@ -9,23 +10,44 @@ import javax.swing.JPanel;
 
 public class Window extends JFrame {
 
+	private static final int searchPanelheight = 130;
+	private static final int infoPanelwidth = 500;
+	private static final int width = 1080;
+	private static final int height = 720;
+
 	private static final long serialVersionUID = 1L;
-	private JPanel panel;
+	private JPanel searchPanel;
+	private JPanel tablePanel;
+	private JPanel infoPanel;
 
 	private JButton button;
 
+	// 1080 720
 	public Window() {
 		super("CS:GO Stats auslesen");
-		panel = new JPanel();
-		panel.setPreferredSize(new Dimension(1080, 720));
-		panel.setBackground(Color.WHITE);
-		panel.setLayout(null);
+		searchPanel = new JPanel();
+		searchPanel.setPreferredSize(new Dimension(width, searchPanelheight));
+		searchPanel.setBackground(Color.GREEN);
+		searchPanel.setLayout(null);
 
-		button = new JButton("Hallo");
-		button.setBounds(250, 200, 200, 50);
-		panel.add(button);
+		button = new JButton("Suche starten");
+		button.setBounds(750, 30, 300, 70);
+		searchPanel.add(button);
 
-		add(panel);
+		add(searchPanel, BorderLayout.NORTH);
+
+		tablePanel = new JPanel();
+		tablePanel.setPreferredSize(new Dimension(width - infoPanelwidth, height - searchPanelheight));
+		tablePanel.setBackground(Color.RED);
+		tablePanel.setLayout(null);
+		add(tablePanel, BorderLayout.EAST);
+
+		infoPanel = new JPanel();
+		infoPanel.setPreferredSize(new Dimension(infoPanelwidth, height - searchPanelheight));
+		infoPanel.setBackground(Color.YELLOW);
+		infoPanel.setLayout(null);
+		add(infoPanel, BorderLayout.WEST);
+
 		setResizable(false);
 		pack();
 		setLocationRelativeTo(null);
