5bb3e1af7dcbd4286280a3ca6294b976a2097e1f
david <david@DerBoss.speedport.ip>
2018-11-23 18:12:00 +0100

    uafadf

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		System.out.println(calculateID("STEAM_0:0:125314026"));
 	}
 	
+	 //CREATED BY MARKUS (PO-)SCHLITZ
+	
 	public static String calculateID(String steamid)
 	{
 		String[] id_split = steamid.split(":");
@@ -19,3 +21,11 @@ public class Main {
 	}
 	
 }
+
+/*
+ *  steamID STEAM_0:0:142231653
+	steamID3 [U:1:284463306]
+		steamID 76561198244729034
+		
+		API KEY 648011BEFF8552A866AAE3F2D64D3AFD
+ */
